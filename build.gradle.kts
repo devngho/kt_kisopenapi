00ba6f407632b93f187ccac285dd8967845f0c8d
devngho <yjh135908@gmail.com>
2022-11-17 01:54:28 +0900

    cio

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,6 +92,7 @@ kotlin {
         }
         val nativeMain by getting {
             dependencies {
+                implementation("io.ktor:ktor-client-cio:$ktorVersion")
                 implementation("io.ktor:ktor-client-curl:$ktorVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-$platform:$coroutineVersion")
                 standard()
@@ -99,6 +100,7 @@ kotlin {
         }
         val nativeTest by getting {
             dependencies {
+                implementation("io.ktor:ktor-client-cio:$ktorVersion")
                 implementation("io.ktor:ktor-client-curl:$ktorVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-$platform:$coroutineVersion")
                 standard()
