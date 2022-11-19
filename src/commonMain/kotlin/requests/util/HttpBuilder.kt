4cacfbaede410a379cecca3b4bbf49b16ba9891e
devngho <yjh135908@gmail.com>
2022-11-19 10:53:06 +0900

    http module improvements

--- src/commonMain/kotlin/requests/util/HttpBuilder.kt
+++ src/commonMain/kotlin/requests/util/HttpBuilder.kt
@@ -1,5 +1,19 @@
 package io.github.devngho.kisopenapi.requests.util
 
+import com.ionspin.kotlin.bignum.serialization.kotlinx.humanReadableSerializerModule
 import io.ktor.client.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
 
-expect fun createHttpClient(): HttpClient
+@OptIn(ExperimentalSerializationApi::class)
+fun createHttpClient() = HttpClient {
+    install(ContentNegotiation) {
+        json(Json {
+            explicitNulls = true
+            serializersModule = humanReadableSerializerModule
+            ignoreUnknownKeys = true
+        })
+    }
+}
