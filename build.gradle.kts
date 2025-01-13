e69280bfdb392a9bdeea6e765f7e941f067bf9d1
devngho <yjh135908@gmail.com>
2025-01-14 02:59:26 +0900

    ⭐ feat: enlarge support platforms

--- build.gradle.kts
+++ build.gradle.kts
@@ -196,81 +196,49 @@ kotlin {
 
 tasks {
     // copied from ionspin/kotlin-multiplatform-bignum (at build.gradle.kts), Apache 2.0
-    // fixed windows part for correct task dependencies
+    // fixed for correct task dependencies in this project
     all {
-        if (hostOs == HostOs.LINUX) {
-            // Linux task dependecies
-
-            // @formatter:off
-            if (this.name.equals("signAndroidNativeArm32Publication")) { this.mustRunAfter("signAndroidNativeArm64Publication") }
-            if (this.name.equals("signAndroidNativeArm64Publication")) { this.mustRunAfter("signAndroidNativeX64Publication") }
-            if (this.name.equals("signAndroidNativeX64Publication")) { this.mustRunAfter("signAndroidNativeX86Publication") }
-            if (this.name.equals("signAndroidNativeX86Publication")) { this.mustRunAfter("signJsPublication") }
-            if (this.name.equals("signJsPublication")) { this.mustRunAfter("signJvmPublication") }
-            if (this.name.equals("signJvmPublication")) { this.mustRunAfter("signKotlinMultiplatformPublication") }
-            if (this.name.equals("signKotlinMultiplatformPublication")) { this.mustRunAfter("signLinuxArm64Publication") }
-            if (this.name.equals("signLinuxArm64Publication")) { this.mustRunAfter("signLinuxX64Publication") }
-            if (this.name.equals("signLinuxX64Publication")) { this.mustRunAfter("signWasmJsPublication") }
-            // @formatter:on
-
-            if (this.name.startsWith("publish")) {
-                this.mustRunAfter("signAndroidNativeArm32Publication")
-                this.mustRunAfter("signAndroidNativeArm64Publication")
-                this.mustRunAfter("signAndroidNativeX64Publication")
-                this.mustRunAfter("signAndroidNativeX86Publication")
-                this.mustRunAfter("signJsPublication")
-                this.mustRunAfter("signJvmPublication")
-                this.mustRunAfter("signKotlinMultiplatformPublication")
-                this.mustRunAfter("signLinuxArm64Publication")
-                this.mustRunAfter("signLinuxX64Publication")
-                this.mustRunAfter("signWasmJsPublication")
-            }
-
-            if (this.name.startsWith("compileTest")) {
-                this.mustRunAfter("signAndroidNativeArm32Publication")
-                this.mustRunAfter("signAndroidNativeArm64Publication")
-                this.mustRunAfter("signAndroidNativeX64Publication")
-                this.mustRunAfter("signAndroidNativeX86Publication")
-                this.mustRunAfter("signJsPublication")
-                this.mustRunAfter("signJvmPublication")
-                this.mustRunAfter("signKotlinMultiplatformPublication")
-                this.mustRunAfter("signLinuxArm64Publication")
-                this.mustRunAfter("signLinuxX64Publication")
-                this.mustRunAfter("signWasmJsPublication")
-            }
-
-            if (this.name.startsWith("linkDebugTest")) {
-                this.mustRunAfter("signAndroidNativeArm32Publication")
-                this.mustRunAfter("signAndroidNativeArm64Publication")
-                this.mustRunAfter("signAndroidNativeX64Publication")
-                this.mustRunAfter("signAndroidNativeX86Publication")
-                this.mustRunAfter("signJsPublication")
-                this.mustRunAfter("signJvmPublication")
-                this.mustRunAfter("signKotlinMultiplatformPublication")
-                this.mustRunAfter("signLinuxArm64Publication")
-                this.mustRunAfter("signLinuxX64Publication")
-                this.mustRunAfter("signWasmJsPublication")
-            }
-        }
-
         if (hostOs == HostOs.MAC) {
-            // Macos task dependencies
+            // macOS task dependencies
             // @formatter:off
-            if (this.name.equals("signIosArm64Publication")) { this.mustRunAfter("signIosSimulatorArm64Publication") }
-            if (this.name.equals("signIosSimulatorArm64Publication")) { this.mustRunAfter("signIosX64Publication") }
-            if (this.name.equals("signIosX64Publication")) { this.mustRunAfter("signMacosArm64Publication") }
-            if (this.name.equals("signMacosArm64Publication")) { this.mustRunAfter("signMacosX64Publication") }
-            if (this.name.equals("signMacosX64Publication")) { this.mustRunAfter("signTvosArm64Publication") }
-            if (this.name.equals("signTvosArm64Publication")) { this.mustRunAfter("signTvosSimulatorArm64Publication") }
-            if (this.name.equals("signTvosSimulatorArm64Publication")) { this.mustRunAfter("signTvosX64Publication") }
-            if (this.name.equals("signTvosX64Publication")) { this.mustRunAfter("signWatchosArm32Publication") }
-            if (this.name.equals("signWatchosArm32Publication")) { this.mustRunAfter("signWatchosArm64Publication") }
-            if (this.name.equals("signWatchosArm64Publication")) { this.mustRunAfter("signWatchosSimulatorArm64Publication") }
-            if (this.name.equals("signWatchosSimulatorArm64Publication")) { this.mustRunAfter("signWatchosX64Publication") }
+            when (this.name) {
+                "signAndroidNativeArm32Publication" -> { this.mustRunAfter("signAndroidNativeArm64Publication") }
+                "signAndroidNativeArm64Publication" -> { this.mustRunAfter("signAndroidNativeX64Publication") }
+                "signAndroidNativeX64Publication" -> { this.mustRunAfter("signAndroidNativeX86Publication") }
+                "signAndroidNativeX86Publication" -> { this.mustRunAfter("signJsPublication") }
+                "signJsPublication" -> { this.mustRunAfter("signJvmPublication") }
+                "signJvmPublication" -> { this.mustRunAfter("signKotlinMultiplatformPublication") }
+                "signKotlinMultiplatformPublication" -> { this.mustRunAfter("signLinuxArm64Publication") }
+                "signLinuxArm64Publication" -> { this.mustRunAfter("signLinuxX64Publication") }
+                "signLinuxX64Publication" -> { this.mustRunAfter("signWasmJsPublication") }
+                "signWasmJsPublication" -> { this.mustRunAfter("signMingwX64Publication") }
+                "signMingwX64Publication" -> { this.mustRunAfter("signIosArm64Publication") }
+                "signIosArm64Publication" -> { this.mustRunAfter("signIosSimulatorArm64Publication") }
+                "signIosSimulatorArm64Publication" -> { this.mustRunAfter("signIosX64Publication") }
+                "signIosX64Publication" -> { this.mustRunAfter("signMacosArm64Publication") }
+                "signMacosArm64Publication" -> { this.mustRunAfter("signMacosX64Publication") }
+                "signMacosX64Publication" -> { this.mustRunAfter("signTvosArm64Publication") }
+                "signTvosArm64Publication" -> { this.mustRunAfter("signTvosSimulatorArm64Publication") }
+                "signTvosSimulatorArm64Publication" -> { this.mustRunAfter("signTvosX64Publication") }
+                "signTvosX64Publication" -> { this.mustRunAfter("signWatchosArm32Publication") }
+                "signWatchosArm32Publication" -> { this.mustRunAfter("signWatchosArm64Publication") }
+                "signWatchosArm64Publication" -> { this.mustRunAfter("signWatchosSimulatorArm64Publication") }
+                "signWatchosSimulatorArm64Publication" -> { this.mustRunAfter("signWatchosX64Publication") }
+            }
             // @formatter:on
 
             if (this.name.startsWith("publish")) {
-                this.mustRunAfter("signIosArm64Publication")
+                this.mustRunAfter("signAndroidNativeArm32Publication")
+                this.mustRunAfter("signAndroidNativeArm64Publication")
+                this.mustRunAfter("signAndroidNativeX64Publication")
+                this.mustRunAfter("signAndroidNativeX86Publication")
+                this.mustRunAfter("signJsPublication")
+                this.mustRunAfter("signJvmPublication")
+                this.mustRunAfter("signKotlinMultiplatformPublication")
+                this.mustRunAfter("signLinuxArm64Publication")
+                this.mustRunAfter("signLinuxX64Publication")
+                this.mustRunAfter("signWasmJsPublication")
+                this.mustRunAfter("signMingwX64Publication")
                 this.mustRunAfter("signIosArm64Publication")
                 this.mustRunAfter("signIosSimulatorArm64Publication")
                 this.mustRunAfter("signIosX64Publication")
@@ -286,7 +254,17 @@ tasks {
             }
 
             if (this.name.startsWith("compileTest")) {
-                this.mustRunAfter("signIosArm64Publication")
+                this.mustRunAfter("signAndroidNativeArm32Publication")
+                this.mustRunAfter("signAndroidNativeArm64Publication")
+                this.mustRunAfter("signAndroidNativeX64Publication")
+                this.mustRunAfter("signAndroidNativeX86Publication")
+                this.mustRunAfter("signJsPublication")
+                this.mustRunAfter("signJvmPublication")
+                this.mustRunAfter("signKotlinMultiplatformPublication")
+                this.mustRunAfter("signLinuxArm64Publication")
+                this.mustRunAfter("signLinuxX64Publication")
+                this.mustRunAfter("signWasmJsPublication")
+                this.mustRunAfter("signMingwX64Publication")
                 this.mustRunAfter("signIosArm64Publication")
                 this.mustRunAfter("signIosSimulatorArm64Publication")
                 this.mustRunAfter("signIosX64Publication")
@@ -300,7 +278,17 @@ tasks {
                 this.mustRunAfter("signWatchosSimulatorArm64Publication")
             }
             if (this.name.startsWith("linkDebugTest")) {
-                this.mustRunAfter("signIosArm64Publication")
+                this.mustRunAfter("signAndroidNativeArm32Publication")
+                this.mustRunAfter("signAndroidNativeArm64Publication")
+                this.mustRunAfter("signAndroidNativeX64Publication")
+                this.mustRunAfter("signAndroidNativeX86Publication")
+                this.mustRunAfter("signJsPublication")
+                this.mustRunAfter("signJvmPublication")
+                this.mustRunAfter("signKotlinMultiplatformPublication")
+                this.mustRunAfter("signLinuxArm64Publication")
+                this.mustRunAfter("signLinuxX64Publication")
+                this.mustRunAfter("signWasmJsPublication")
+                this.mustRunAfter("signMingwX64Publication")
                 this.mustRunAfter("signIosArm64Publication")
                 this.mustRunAfter("signIosSimulatorArm64Publication")
                 this.mustRunAfter("signIosX64Publication")
@@ -313,10 +301,8 @@ tasks {
                 this.mustRunAfter("signWatchosArm64Publication")
                 this.mustRunAfter("signWatchosSimulatorArm64Publication")
             }
-        }
-
-        if (hostOs == HostOs.WINDOWS) {
-            // Windows task dependecies
+        } else {
+            // Windows || linux task dependecies
 
             // @formatter:off
             if (this.name.equals("signAndroidNativeArm32Publication")) { this.mustRunAfter("signAndroidNativeArm64Publication") }
